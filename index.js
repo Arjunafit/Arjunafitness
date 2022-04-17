78f286e5660431c162392845208b247bc6430388
arjunafit <arjunafitness@gmail.com>
2022-04-16 21:39:48 -0500

    actualizacion

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+$(document).ready(function () {
+    $(".btn-mobile").click(function () {
+      $(".menu-mobile").slideToggle("slow");
+    });
+  });
+
+
 $(document).ready(function (){
     if(!$("#myCanvas").tagcanvas({
-        textColour: "#FFF",
+        textColour: "#08fdd8",
         outlineColour: "transparent",
         reverse: true,
         depth: 0.8,
         maxSpeed: 0.05,
         weight: true,
-        
     }, "tags")){
+        // something went wrong hide the canvas container,
+        $("#myCanvasContainer");
     }
 })
