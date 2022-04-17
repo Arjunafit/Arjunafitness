f8eb1cc15a4958b4b373804e8f81c0ebf1313341
arjunafit <arjunafitness@gmail.com>
2022-04-17 02:24:11 -0500

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-$(document).ready(function () {
-    $(".btn-mobile").click(function () {
-      $(".menu-mobile").slideToggle("slow");
-    });
-  });
-
-
 $(document).ready(function (){
-    if(!$("#myCanvas").tagcanvas({
-        textColour: "#08fdd8",
-        outlineColour: "transparent",
-        reverse: true,
-        depth: 0.8,
-        maxSpeed: 0.05,
-        weight: true,
-    }, "tags")){
-        // something went wrong hide the canvas container,
-        $("#myCanvasContainer");
-    }
+  if(!$("#myCanvas").tagcanvas({
+      textColour: "#fff",
+      outlineColour: "transparent",
+      reverse: true,
+      depth: 0.8,
+      maxSpeed: 0.05,
+      weight: true,
+  }, "tags")){
+      // something went wrong hide the canvas container,
+      $("#myCanvasContainer");
+  }
 })
